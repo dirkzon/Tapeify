0413c2c9397f0cddb9c5fbce891c47408557312c
Dirk van Zon <dirk@moeilijkedingen.nl>
2025-01-24 12:54:24 +0100

    removed logs

--- src/views/CallbackView.vue
+++ src/views/CallbackView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { useAuthStore } from '@/stores/auth'
-import { useCookies } from "vue3-cookies";
+import { useCookies } from 'vue3-cookies'
 import { useRouter } from 'vue-router'
-import { useProfileStore } from '@/stores/profile';
+import { useProfileStore } from '@/stores/profile'
 
 const authStore = useAuthStore()
 const profileStore = useProfileStore()
@@ -12,17 +12,13 @@ const router = useRouter()
 
 onMounted(async () => {
   const code = new URL(location.href).searchParams.get('code')
-  
-  const { access_token, refresh_token } = await authStore.requestAccessToken(String(code))
-  console.log(refresh_token)
-  cookies.set('access_token', access_token, 3600)
-  cookies.set("refresh_token", refresh_token, '1d')
 
-  console.log(cookies.get('access_token'))
-  console.log(cookies.get('refresh_token'))
+  const { access_token, refresh_token } = await authStore.requestAccessToken(String(code))
+  cookies.set('access_token', access_token, 3600)
+  cookies.set('refresh_token', refresh_token, '1d')
 
   profileStore.getProfile()
-  router.push({path: '/'})
+  router.push({ path: '/' })
 })
 </script>
 
