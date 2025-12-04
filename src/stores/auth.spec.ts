811dd2703b3e965bd9d707afa385377bdccf75a3
Dirk van Zon <dirk.v.zon@outlook.com>
2025-12-04 20:02:27 +0100

    Add refresh token test

--- src/stores/auth.spec.ts
+++ src/stores/auth.spec.ts
@@ -63,4 +63,33 @@ describe('Auth store', () => {
       expect(authStore.expiresAt).toEqual(expectedTime);
     });
   });
+  describe('refreshAccessToken', () => {
+    it('successfully refresh access token and calls API with correct args', async () => {
+      const authStore = useAuthStore();
+
+      authStore.refreshToken = '9876543210';
+
+      expect(authStore.accessToken).not.toBeDefined();
+      expect(authStore.refreshToken).toBeDefined();
+      expect(authStore.expiresAt).not.toBeDefined();
+
+      await authStore.refreshAccessToken();
+
+      expect(postSpy).toHaveBeenCalledTimes(1);
+      expect(postSpy).toHaveBeenCalledWith(
+        '/api/token',
+        'grant_type=refresh_token&refresh_token=9876543210&client_id=0123456789',
+        {
+          headers: {
+            Authorization: 'Basic MDEyMzQ1Njc4OTo3NGJkNDY2NTE1MDQ0M2QyYmY0MzhhNGM0YTViYmNiNw=='
+          }
+        }
+      );
+
+      expect(authStore.accessToken).toEqual(tokenResponseMock.access_token);
+      expect(authStore.refreshToken).toEqual(tokenResponseMock.refresh_token);
+      const expectedTime = mockTime.getTime() + tokenResponseMock.expires_in * 1000;
+      expect(authStore.expiresAt).toEqual(expectedTime);
+    });
+  });
 });
