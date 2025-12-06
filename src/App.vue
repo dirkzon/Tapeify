9c316d526de43be75d0b723ae99565b8a150e8e2
Dirk van Zon <dirk.v.zon@outlook.com>
2025-12-06 13:57:13 +0100

    Change deploy workflow to run on new release. And inject release tag as environment variable in site.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<script lang="ts" setup>
+const releaseTag = (import.meta.env.VITE_RELEASE_TAG as string) || '';
+const releaseUrl = `https://github.com/dirkzon/Tapeify/releases/tag/${releaseTag}`
+</script>
+
 <template>
   <v-app>
     <v-container fill-height fluid>
@@ -11,11 +16,12 @@
           href="https://github.com/dirkzon/Tapeify" target="_blank">
           <span class="mt-auto">github</span>
         </v-btn>
+
+        <v-btn v-if="releaseTag"  class="me-2 text-none" prepend-icon="mdi-tag-outline" density="comfortable" variant="plain"
+          :href="releaseUrl" target="_blank">
+          <span class="mt-auto">{{ releaseTag }}</span>
+        </v-btn>
       </div>
     </v-footer>
   </v-app>
 </template>
-
-<script lang="ts" setup>
-//
-</script>
