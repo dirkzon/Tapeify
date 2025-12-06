4c395a085d82c390041b8210209a3ec829b4ee02
Dirk van Zon <dirk.v.zon@outlook.com>
2025-12-06 12:31:12 +0100

    Add footer with repo link

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@
         <router-view />
       </v-row>
     </v-container>
+    <v-footer class="text-center d-flex flex-column ga-2 py-4 ">
+      <div class="d-flex h-100 align-end justify-center">
+        <v-btn class="me-2 text-none" prepend-icon="mdi-github" density="comfortable" variant="plain"
+          href="https://github.com/dirkzon/Tapeify" target="_blank">
+          <span class="mt-auto">GitHub</span>
+        </v-btn>
+      </div>
+    </v-footer>
   </v-app>
 </template>
 
