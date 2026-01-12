eda01dfac005ff7185efee8e92c944aa99850bdc
Dirk van Zon <dirk.v.zon@outlook.com>
2026-01-12 20:17:05 +0100

    Use unavailable track ids for the draggable list

--- src/components/UnavailableTracksList.vue
+++ src/components/UnavailableTracksList.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 import { useTracksStore } from '@/stores/tracks';
-import type { Track } from '@/types/tapeify/models';
 
 const tracksStore = useTracksStore()
 
@@ -10,6 +9,7 @@ function onChanged(changeEvent: any) {
         case 'moved':
             break;
         case 'added':
+            console.log(changeEvent.added.element)
             tracksStore.MarkTrackAsUnavailable(changeEvent.added.element)
             break
         case 'removed':
@@ -24,7 +24,7 @@ function onChanged(changeEvent: any) {
         <v-list-subheader>
             Unused Tracks
         </v-list-subheader>
-        <draggable :list="tracksStore.unavailableTracks" group="tracks" item-key="id" animation="200" @change="onChanged">
+        <draggable :list="tracksStore.unavailableTrackIds" group="tracks" item-key="id" animation="200" @change="onChanged">
             <v-list-item v-for="track in tracksStore.unavailableTracks" :key="track.id" :value="track.id" active-class="text-secondary">
                 {{ track.name }}
             </v-list-item>
--- src/stores/tracks.ts
+++ src/stores/tracks.ts
@@ -4,17 +4,17 @@ import { defineStore } from 'pinia'
 export const useTracksStore = defineStore('tracks', {
   state: () => ({
     _masterTrackList: [] as Track[],
-    _unavailableTrackIds: [] as string[],
+    unavailableTrackIds: [] as string[],
   }),
   getters: {
     availableTracksTotalDuration(): number {
       return this.availableTracks.reduce((acc, track) => acc + track.durationMs, 0)
     },
     availableTracks(state): Track[] {
-      return state._masterTrackList.filter(track => !state._unavailableTrackIds.includes(track.id))
+      return state._masterTrackList.filter(track => !state.unavailableTrackIds.includes(track.id))
     },
     unavailableTracks(state): Track[] {
-      return state._masterTrackList.filter(track => state._unavailableTrackIds.includes(track.id))
+      return state._masterTrackList.filter(track => state.unavailableTrackIds.includes(track.id))
     }
   },
   actions: {
@@ -31,12 +31,12 @@ export const useTracksStore = defineStore('tracks', {
       return this._masterTrackList.some(track => track.id === trackId)
     },
     MarkTrackAsUnavailable(trackId: string) {
-      if (!this._unavailableTrackIds.includes(trackId) && this.TrackExists(trackId)) {
-        this._unavailableTrackIds.push(trackId)
+      if (!this.unavailableTrackIds.includes(trackId) && this.TrackExists(trackId)) {
+        this.unavailableTrackIds.push(trackId)
       }
     },
     MarkTrackAsAvailable(trackId: string) {
-      this._unavailableTrackIds = this._unavailableTrackIds.filter(id => id !== trackId)
+      this.unavailableTrackIds = this.unavailableTrackIds.filter(id => id !== trackId)
     },
   }
 })
