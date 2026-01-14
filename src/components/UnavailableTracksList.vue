649bb7a461c25ee2f5c5deb3788024ff91ef37a2
Dirk van Zon <dirk.v.zon@outlook.com>
2026-01-14 20:27:14 +0100

    Add sheet with secondary color to background

--- src/components/UnavailableTracksList.vue
+++ src/components/UnavailableTracksList.vue
@@ -31,29 +31,32 @@ function trackClicked(trackId: string) {
 </script>
 
 <template>
-    <v-list select-strategy="leaf" v-model:selected="tracksStore.unavailableTracks" color="secondary">
-        <v-list-subheader class="text-subtitle-1" style="color: inherit">
-            Unused Tracks
-        </v-list-subheader>
-        <draggable :list="tracksStore.unavailableTrackIds" group="tracks" item-key="id" animation="200"
-            @change="onChanged" :sort="false" class="drag-container">
-            <v-list-item v-for="track in unavailableTracks" :key="track.id" :value="track.id"
-                v-on:click="trackClicked(track.id)" class="pa-1">
-                <template v-slot:prepend>
-                    <v-icon class="drag-handle" icon="mdi-drag-vertical" />
-                    <v-avatar tile>
-                        <v-img v-if="track.image" :src="track.image.href" />
-                        <v-icon v-else icon="mdi-music" />
-                    </v-avatar>
-                </template>
-                <v-list-item-title :title="track.name">{{ track.name }}</v-list-item-title>
-                <v-list-item-subtitle :title="track.artists.join()">{{ track.artists.join() }}</v-list-item-subtitle>
-                <template v-slot:append>
-                    <v-icon icon="mdi-restore" />
-                </template>
-            </v-list-item>
-        </draggable>
-    </v-list>
+    <v-sheet color="secondary":rounded="true" class="pa-1">
+        <v-list select-strategy="leaf" v-model:selected="tracksStore.unavailableTracks" color="secondary">
+            <v-list-subheader class="text-subtitle-1" style="color: inherit">
+                Unused Tracks
+            </v-list-subheader>
+            <draggable :list="tracksStore.unavailableTrackIds" group="tracks" item-key="id" animation="200"
+                @change="onChanged" :sort="false" class="drag-container">
+                <v-list-item v-for="track in unavailableTracks" :key="track.id" :value="track.id"
+                    v-on:click="trackClicked(track.id)" class="pa-1">
+                    <template v-slot:prepend>
+                        <v-icon class="drag-handle" icon="mdi-drag-vertical" />
+                        <v-avatar tile>
+                            <v-img v-if="track.image" :src="track.image.href" />
+                            <v-icon v-else icon="mdi-music" />
+                        </v-avatar>
+                    </template>
+                    <v-list-item-title :title="track.name">{{ track.name }}</v-list-item-title>
+                    <v-list-item-subtitle :title="track.artists.join()">{{ track.artists.join()
+                        }}</v-list-item-subtitle>
+                    <template v-slot:append>
+                        <v-icon icon="mdi-restore" />
+                    </template>
+                </v-list-item>
+            </draggable>
+        </v-list>
+    </v-sheet>
 </template>
 
 <style scoped>
