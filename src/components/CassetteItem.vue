ab00ca194a4a64d029c6ef19a3229fb9646b4c8f
Dirk van Zon <dirk.v.zon@outlook.com>
2026-01-24 12:05:05 +0100

    Add lock button that appears when item is anchored or hovered over.

--- src/components/CassetteItem.vue
+++ src/components/CassetteItem.vue
@@ -1,48 +1,63 @@
 <script lang="ts" setup>
+import { useAnchorsStore } from '@/stores/anchor';
 import { useTracksStore } from '@/stores/tracks';
 import { formatDuration } from '@/utils/duration/durationHelper';
 
 const props = defineProps<{
     trackId: string,
-    clicked: Function
+    onLockClick: Function
 }>()
 
 const tracksStore = useTracksStore()
+const anchorStore = useAnchorsStore()
 
 const track = computed(() => tracksStore.GetTrackById(props.trackId))
+const anchored = computed(() => anchorStore.isTrackAnchored(props.trackId))
+const anchorIcon = computed(() => {
+    if (anchorStore.isTrackAnchored(props.trackId)) {
+        return "mdi-lock"
+    } else {
+        return "mdi-lock-open"
+    }
+})
 </script>
 
 <template>
-    <v-list-item active-class="text-secondary" class="py-2" handle=".drag-handle" @click="clicked()">
-        <template v-slot:prepend>
-            <v-icon class="drag-handle" icon="mdi-drag-vertical" size="large">
-            </v-icon>
-            <v-avatar tile>
-                <v-img v-if="track?.image" :src="track.image.href" />
-                <v-icon v-else icon="mdi-music" />
-            </v-avatar>
+    <v-hover>
+        <template v-slot:default="{ isHovering, props }">
+            <v-list-item active-class="text-secondary" class="py-2" handle=".drag-handle" v-bind="props">
+                <template v-slot:prepend>
+                    <v-icon class="drag-handle" icon="mdi-drag-vertical" size="large">
+                    </v-icon>
+                    <v-avatar tile>
+                        <v-img v-if="track?.image" :src="track.image.href" />
+                        <v-icon v-else icon="mdi-music" />
+                    </v-avatar>
+                </template>
+                <v-list-item-title :title="track?.name">{{ track?.name }}</v-list-item-title>
+                <v-list-item-subtitle :title="track?.artists.join()">{{ track?.artists.join() }}</v-list-item-subtitle>
+                <template v-slot:append>
+                    <div class="track-meta d-flex align-center">
+                        <v-btn v-if="isHovering || anchored" :icon="anchorIcon" size="x-small" variant="text"
+                            @click="onLockClick" />
+                        <div class="text-subtitle-1">{{ formatDuration(track?.durationMs) }}</div>
+                    </div>
+                </template>
+            </v-list-item>
         </template>
-        <v-list-item-title :title="track?.name">{{ track?.name }}</v-list-item-title>
-        <v-list-item-subtitle :title="track?.artists.join()">{{ track?.artists.join() }}</v-list-item-subtitle>
-        <template v-slot:append="{ isSelected }">
-            <div class="track-meta d-flex align-center">
-                <v-icon v-if="isSelected" size="16">mdi-lock</v-icon>
-                <div class="text-subtitle-1">{{ formatDuration(track?.durationMs) }}</div>
-            </div>
-        </template>
-    </v-list-item>
+    </v-hover>
 </template>
 
 
 <style scoped>
 .drag-handle {
-  cursor: grab;
-  margin-right: 10px;
-  color: #999;
-  user-select: none;
+    cursor: grab;
+    margin-right: 10px;
+    color: #999;
+    user-select: none;
 }
 
 .drag-handle:active {
-  cursor: grabbing;
+    cursor: grabbing;
 }
 </style>
