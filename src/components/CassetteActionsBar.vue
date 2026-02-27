e70b465a16be663c644ee1504d94838ad335b5c0
Dirk van Zon <dirk.v.zon@outlook.com>
2026-02-27 11:43:34 +0100

    Clear all selected tracks on bulk (un)anchor to prevent selected track being scattered

--- src/components/CassetteActionsBar.vue
+++ src/components/CassetteActionsBar.vue
@@ -2,7 +2,7 @@
 import { useTracksStore } from '@/stores/tracks';
 import { useAnchorsStore } from '@/stores/anchor';
 import { useSortingStore } from '@/stores/sorting';
-
+import { useHotkey } from 'vuetify/dist/vuetify.js';
 
 const route = useRoute()
 
@@ -13,6 +13,9 @@ const anchorStore = useAnchorsStore()
 const sortStore = useSortingStore()
 
 const multipleSelected = computed(() => tracksStore.selectedTracks.length > 1)
+
+useHotkey('ctrl+l', bulkAnchor)
+useHotkey('ctrl+u', bulkUnanchor)
 
 function bulkAnchor() {
     for (const id of tracksStore.selectedTracks) {
@@ -28,12 +31,16 @@ function bulkAnchor() {
             positionIndex: trackLayout.position
         })
     }
+    sortStore.sortTracks()
+    tracksStore.ClearSelectedTracks()
 }
 
 function bulkUnanchor() {
     for (const id of tracksStore.selectedTracks) {
         anchorStore.removeAnchor(id)
     }
+    sortStore.sortTracks()
+    tracksStore.ClearSelectedTracks()
 }
 
 </script>
@@ -41,10 +48,23 @@ function bulkUnanchor() {
 <template>
     <v-app-bar v-if="showBar" class="included">
         <template v-slot:append>
-            <v-btn icon="mdi-lock-open-remove" :disabled="!multipleSelected" @click="bulkUnanchor"></v-btn>
+            <v-btn icon @click="bulkUnanchor" :disabled="!multipleSelected">
+                <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                        <v-icon v-on="on">mdi-lock-open-remove</v-icon>
+                    </template>
+                    <span>Unanchor (Ctrl + U)</span>
+                </v-tooltip>
+            </v-btn>
 
-            <v-btn icon="mdi-lock-plus" :disabled="!multipleSelected" @click="bulkAnchor"></v-btn>
-
+            <v-btn icon @click="bulkAnchor" :disabled="!multipleSelected" class="ml-4">
+                <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                        <v-icon v-on="on">mdi-lock-plus</v-icon>
+                    </template>
+                    <span>Anchor (Ctrl + L)</span>
+                </v-tooltip>
+            </v-btn>
         </template>
     </v-app-bar>
 </template>
