10c0d8a760d84c8e899a7bb6d7682aaeb7a95745
Dirk van Zon <dirk.v.zon@outlook.com>
2026-03-05 14:15:07 +0100

    Added debouncing to playlist search tab

--- src/components/SearchPlaylistsTab.vue
+++ src/components/SearchPlaylistsTab.vue
@@ -2,104 +2,118 @@
 import router from '@/router'
 import { usePlaylistsStore } from '@/stores/playlists';
 import type { PlaylistSearchResult } from '@/types/tapeify/models';
+import debounce from 'lodash/debounce';
 import type { InfiniteScrollSide, InfiniteScrollStatus } from 'vuetify/lib/components/VInfiniteScroll/VInfiniteScroll.mjs';
 
 const playlistsStore = usePlaylistsStore()
 
 let offset = ref(0)
 let limit = ref(10)
-let query = ref('')
 const loading = ref(false)
 const playlists = ref<PlaylistSearchResult>({
-    playlists: [],
-    next: false,
-    previous: false
+  playlists: [],
+  next: false,
+  previous: false
 })
-const tab = 'search_playlists'
 
+const TAB_NAME = 'search_playlists'
+const DEBOUNCE_DELAY_MS = 200
+
+let query = ref('')
+watch(query, (newQuery) => {
+  if (newQuery.length === 0) {
+    playlists.value.playlists = []
+    updateUrl()
+    return
+  }
+  loading.value = true
+  searchPlaylists(newQuery, limit.value, offset.value)
+  loading.value = false
+})
 
 onMounted(async () => {
-    const url = new URL(location.href)
+  const url = new URL(location.href)
 
-    const queryParam = url.searchParams.get('query')
-    const tabParam = url.searchParams.get('tab')
+  const queryParam = url.searchParams.get('query')
+  const tabParam = url.searchParams.get('tab')
 
-    if (queryParam !== null && tabParam === tab) {
-        query.value = queryParam
-        playlists.value = await playlistsStore.searchPlaylists(
-            query.value,
-            limit.value,
-            offset.value
-        )
-    }
+  if (queryParam !== null && tabParam === TAB_NAME) {
+    query.value = queryParam
+    playlists.value = await playlistsStore.searchPlaylists(
+      query.value,
+      limit.value,
+      offset.value
+    )
+  }
 })
 
-async function searchPlaylists() {
-    updateUrl()
-    if (query.value === '') {
-        playlists.value.playlists = []
-    } else {
-        loading.value = true
-        offset.value = 0
-        playlists.value = await playlistsStore.searchPlaylists(
-            query.value,
-            limit.value,
-            offset.value
-        ).finally(() => loading.value = false)
-    }
-}
+const searchPlaylists = debounce(async (query: string, limit: number = 10, offset: number = 0) => {
+  updateUrl()
+  if (query === '') {
+    playlists.value.playlists = []
+  } else {
+    loading.value = true
+    offset = 0
+    playlists.value = await playlistsStore.searchPlaylists(
+      query,
+      limit,
+      offset
+    ).finally(() => loading.value = false)
+  }
+
+}, DEBOUNCE_DELAY_MS)
 
 async function LoadMorePlaylists({ side, done }: { side: InfiniteScrollSide; done: (status: InfiniteScrollStatus) => void }) {
-    if (query.value === '') {
-        playlists.value.playlists = []
-        return
+  if (query.value === '') {
+    playlists.value.playlists = []
+    return
+  }
+  updateUrl()
+  offset.value += limit.value
+  done('loading')
+  await playlistsStore.searchPlaylists(
+    query.value,
+    limit.value,
+    offset.value
+  ).then((newPlaylists) => {
+    playlists.value.playlists.push(...newPlaylists.playlists)
+    if (!newPlaylists.next) {
+      done('empty')
+    } else {
+      done('ok')
     }
-    updateUrl()
-    offset.value += limit.value
-    done('loading')
-    await playlistsStore.searchPlaylists(
-        query.value,
-        limit.value,
-        offset.value
-    ).then((newPlaylists) => {
-        playlists.value.playlists.push(...newPlaylists.playlists)
-        if (!newPlaylists.next) {
-            done('empty')
-        } else {
-            done('ok')
-        }
-    }).catch(() => done('error'))
+  }).catch(() => done('error'))
 }
 
 function ClearSearchBar() {
-    playlists.value.playlists = []
-    offset.value = 0
-    query.value = ''
-    updateUrl()
+  playlists.value.playlists = []
+  offset.value = 0
+  query.value = ''
+  updateUrl()
 }
 
 function updateUrl() {
-    if (query.value === '') {
-        router.push({
-            name: '/HomeView',
-        });
-    } else {
-        router.push({
-            name: '/HomeView',
-            query: {
-                query: query.value,
-                tab: tab
-            }
-        });
-    }
+  if (query.value === '') {
+    router.push({
+      name: '/HomeView',
+    });
+  } else {
+    router.push({
+      name: '/HomeView',
+      query: {
+        query: query.value,
+        tab: TAB_NAME
+      }
+    });
+  }
 }
 </script>
 
 <template>
-    <v-card flat>
-        <v-text-field v-model:model-value="query" label="Search playlists on Spotify" append-inner-icon="mdi-magnify"
-            :loading="loading" dense hide-details @keydown.enter="searchPlaylists" @update:model-value="searchPlaylists"
-            @click:clear="ClearSearchBar" :clearable="true"/>
-        <PlaylistList :playlists="playlists.playlists" :load="LoadMorePlaylists" />
-    </v-card>
+  <v-card flat>
+    <v-text-field v-model:model-value="query" label="Search playlists on Spotify" append-inner-icon="mdi-magnify"
+      :loading="loading" dense hide-details @keydown.enter="searchPlaylists(query, limit, offset)" clearable
+      @click:clear="ClearSearchBar" />
+    <PlaylistList :playlists="playlists.playlists" :load="LoadMorePlaylists" />
+  </v-card>
 </template>
